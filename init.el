b86a33a92e56af8775065efa81c5bda3c1209f8d
Stefan <falken@umich.edu>
2018-12-05 13:08:15 -0500

    more stuff

--- init.el
+++ init.el
@@ -13,6 +13,32 @@
 (define-key evil-insert-state-map "jk" 'evil-normal-state)
 (define-key evil-normal-state-map (kbd "C-u") 'evil-scroll-up)
 
+;;;; This snippet enables lua-mode
+
+;; This line is not necessary, if lua-mode.el is already on your load-path
+(add-to-list 'load-path "~/.emacs.d/lua-mode/")
+
+(autoload 'lua-mode "lua-mode" "Lua editing mode." t)
+(add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
+(add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
+
+(add-to-list 'load-path "~/.emacs.d/arduino-mode")
+(add-to-list 'auto-mode-alist '("\\.\\(pde\\|ino\\)$" . arduino-mode))
+;; (setq auto-mode-alist (cons '("\\.\\(pde\\|ino\\)$" . arduino-mode) auto-mode-alist))
+(autoload 'arduino-mode "arduino-mode" "Arduino editing mode." t)
+
+(setq-default evil-shift-width 2)
+;; (setq-default visible-bell 1)
+(setq-default ring-bell-function
+  (lambda ()
+    (let
+      ((orig-fg (face-foreground 'mode-line)))
+          (set-face-foreground 'mode-line "#F2804F")
+          (run-with-idle-timer 0.1 nil
+                               (lambda (fg) (set-face-foreground 'mode-line fg))
+                               orig-fg)))
+)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -21,7 +47,7 @@
  '(custom-enabled-themes (quote (wombat)))
  '(package-selected-packages
    (quote
-    (evil-commentary rust-mode dumb-jump flycheck-pos-tip flycheck evil)))
+    (magit markdown-mode evil-commentary rust-mode dumb-jump flycheck-pos-tip flycheck evil)))
  '(scroll-bar-mode nil)
  '(tool-bar-mode nil))
 (custom-set-faces
